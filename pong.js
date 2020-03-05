c5865f2a742f3d423bdba2ad3fe9e80eff2be9be
Zach <zach.hathaway48@gmail.com>
2020-03-04 20:47:40 -0600

    Added user paddle to pong.

--- pong.js
+++ pong.js
@@ -17,15 +17,23 @@ var playBall = function() {
 
 }
 
-$(document).ready(function() {
+var startBall = function() {
   var animationCount = 0;
-  document.querySelector("#myNameRow").addEventListener("animationend", () => {
-    document.querySelector("body").addEventListener("mousemove", () => {
-      var rect = document.querySelector("#myName").getBoundingClientRect();
-      var updateVal = rect.x - event.clientY + rect.height/2;
-      document.querySelector("#myName").style.transform = "translateX(" + updateVal + "px)";
+  // document.querySelector("#myNameRow").addEventListener("animationend", () => {
+  //   document.querySelector("body").addEventListener("mousemove", () => {
+  //     var rect = document.querySelector("#myName").getBoundingClientRect();
+  //     var updateVal = rect.x - event.clientY + rect.height/2;
+  //     document.querySelector("#myName").style.transform = "translateX(" + updateVal + "px)";
+  //
+  //   });
 
-    });
+    document.querySelector("#iconPaddle").addEventListener("animationend", () => {
+      document.querySelector("#icp").classList.remove("text-center", "mt-5");
+      document.querySelector("body").addEventListener("mousemove", () => {
+        var rect = document.querySelector("#icp").getBoundingClientRect();
+        var updateVal = event.clientY - rect.x/2 - rect.width/2;
+        document.querySelector("#icp").style.transform = "translateX(" + updateVal + "px)";
+      });
 });
 
   document.querySelector("#ball").addEventListener("animationend", () => {
@@ -36,8 +44,11 @@ $(document).ready(function() {
   $("#myNameRow").css({"--maxRot": "-90deg", "--xp": "-50%", "--yp": "0%"});
   $("#myNameRow").addClass("paddleAnimate");
 
-  $("#iconPaddle").css({"--maxRot": "90deg", "--xp": "50%", "--yp": "25%"});
+  $("#iconPaddle").css({"--maxRot": "90deg", "--xp": "50%", "--yp": "0%"});
   $("#iconPaddle").addClass("paddleAnimate");
+}
+
+$(document).ready(function() {
 
 });
 
